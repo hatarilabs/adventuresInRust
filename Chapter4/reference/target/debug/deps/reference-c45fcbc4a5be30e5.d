4fd240da06d13ef84453cb860048ff86504f0a35
Hatari Labs <hatariresearchlabs@gmail.com>
2025-03-23 19:46:28 -0500

    a
    a

---
+++ Chapter4/reference/target/debug/deps/reference-c45fcbc4a5be30e5.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/GIDA/Documents/GitHub/adventuresInRust/Chapter4/reference/target/debug/deps/reference-c45fcbc4a5be30e5: src/main.rs
+
+/mnt/c/Users/GIDA/Documents/GitHub/adventuresInRust/Chapter4/reference/target/debug/deps/reference-c45fcbc4a5be30e5.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter4/reference/target/debug/reference.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/GIDA/Documents/GitHub/adventuresInRust/Chapter4/reference/target/debug/reference: /mnt/c/Users/GIDA/Documents/GitHub/adventuresInRust/Chapter4/reference/src/main.rs
